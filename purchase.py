e6de5f00a767884c5a814136774f04a34b8f4765
Raimon Esteve <raimonesteve@gmail.com>
2023-11-16 19:23:54 +0100

    gross_unit_price use same digits that unit_price (#5)
    #163307

--- purchase.py
+++ purchase.py
@@ -5,7 +5,9 @@ from decimal import Decimal
 from trytond.model import fields
 from trytond.pool import PoolMeta
 from trytond.pyson import Eval
-from trytond.config import config as config_
+from trytond.modules.currency.fields import Monetary
+from trytond.modules.account_invoice_discount.invoice import (gross_unit_price_digits,
+    discount_digits)
 from trytond.modules.product import round_price
 
 STATES = {
@@ -13,45 +15,42 @@ STATES = {
     'required': Eval('type') == 'line',
     'readonly': Eval('purchase_state') != 'draft',
     }
-DIGITS = config_.getint('product', 'price_decimal', default=4)
-DISCOUNT_DIGITS = config_.getint('product', 'discount_decimal', default=4)
 
 
 class PurchaseLine(metaclass=PoolMeta):
     __name__ = 'purchase.line'
 
-    gross_unit_price = fields.Numeric('Gross Price', digits=(16, DIGITS),
-        states=STATES)
-    gross_unit_price_wo_round = fields.Numeric('Gross Price without rounding',
-        digits=(16, DIGITS + DISCOUNT_DIGITS), readonly=True)
-    discount = fields.Numeric('Discount', digits=(16, DISCOUNT_DIGITS),
+    gross_unit_price = Monetary('Gross Price', digits=gross_unit_price_digits,
+        currency='currency', states=STATES)
+    discount = fields.Numeric('Discount', digits=discount_digits,
         states=STATES)
 
     @classmethod
     def __setup__(cls):
-        super(PurchaseLine, cls).__setup__()
+        super().__setup__()
         cls.unit_price.states['readonly'] = True
-        cls.unit_price.digits = (20, DIGITS + DISCOUNT_DIGITS)
 
     @staticmethod
     def default_discount():
         return Decimal(0)
 
-    @fields.depends('gross_unit_price', 'discount',
+    @fields.depends('gross_unit_price', 'unit_price', 'discount',
         methods=['on_change_with_amount'])
     def update_prices(self):
         unit_price = None
-        gross_unit_price = gross_unit_price_wo_round = self.gross_unit_price
+        gross_unit_price = self.gross_unit_price
         if self.gross_unit_price is not None and self.discount is not None:
             unit_price = self.gross_unit_price * (1 - self.discount)
             unit_price = round_price(unit_price)
 
             if self.discount != 1:
-                gross_unit_price_wo_round = unit_price / (1 - self.discount)
-            gross_unit_price = round_price(gross_unit_price_wo_round)
+                gross_unit_price = unit_price / (1 - self.discount)
+
+            gup_digits = self.__class__.gross_unit_price.digits[1]
+            gross_unit_price = gross_unit_price.quantize(
+                Decimal(str(10.0 ** -gup_digits)))
 
         self.gross_unit_price = gross_unit_price
-        self.gross_unit_price_wo_round = gross_unit_price_wo_round
         self.unit_price = unit_price
         self.amount = self.on_change_with_amount()
 
@@ -63,13 +62,19 @@ class PurchaseLine(metaclass=PoolMeta):
     def on_change_gross_unit_price(self):
         return self.update_prices()
 
+    @fields.depends('unit_price', methods=['update_prices'])
+    def on_change_unit_price(self):
+        # unit_price has readonly state but could set unit_price from source code
+        if self.unit_price is not None:
+            self.update_prices()
+
     @fields.depends(methods=['update_prices'])
     def on_change_discount(self):
         return self.update_prices()
 
     @fields.depends('unit_price', 'discount', methods=['update_prices'])
     def on_change_product(self):
-        super(PurchaseLine, self).on_change_product()
+        super().on_change_product()
         self.gross_unit_price = self.unit_price
         if not self.discount:
             self.discount = Decimal(0)
@@ -79,7 +84,7 @@ class PurchaseLine(metaclass=PoolMeta):
 
     @fields.depends('unit_price', 'discount', methods=['update_prices'])
     def on_change_quantity(self):
-        super(PurchaseLine, self).on_change_quantity()
+        super().on_change_quantity()
         self.gross_unit_price = self.unit_price
         if not self.discount:
             self.discount = Decimal(0)
@@ -88,7 +93,7 @@ class PurchaseLine(metaclass=PoolMeta):
             self.update_prices()
 
     def get_invoice_line(self):
-        lines = super(PurchaseLine, self).get_invoice_line()
+        lines = super().get_invoice_line()
         for line in lines:
             line.gross_unit_price = self.gross_unit_price
             line.discount = self.discount
@@ -107,4 +112,4 @@ class PurchaseLine(metaclass=PoolMeta):
             vals['gross_unit_price'] = round_price(gross_unit_price)
             if not vals.get('discount'):
                 vals['discount'] = Decimal(0)
-        return super(PurchaseLine, cls).create(vlist)
+        return super().create(vlist)
