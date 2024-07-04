f6147dbdd4fbc440da604a5342ac2d5e9dfdc274
Albert Cervera i Areny <albert@nan-tic.com>
2024-07-05 01:02:53 +0200

    Refactor based on core's sale_discount design.

--- tests/test_module.py
+++ tests/test_module.py
@@ -1,13 +1,12 @@
-
-# This file is part of Tryton.  The COPYRIGHT file at the top level of
-# this repository contains the full copyright notices and license terms.
-
+# This file is part purchase_discount module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 from trytond.modules.company.tests import CompanyTestMixin
 from trytond.tests.test_tryton import ModuleTestCase
 
 
 class PurchaseDiscountTestCase(CompanyTestMixin, ModuleTestCase):
-    'Test PurchaseDiscount module'
+    'Test Purchase Discount module'
     module = 'purchase_discount'
 
 
